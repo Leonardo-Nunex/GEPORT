9e61344f3d0911fa2112ef8305c266456244e1bd
Jhonatasop <jhonatasaraujo08@outlook.com>
2023-11-16 21:12:28 -0300

    update trazerDados.php
    correção do código para conseguir trazer dados do banco. (teste)

--- profile/trazerDados.php
+++ profile/trazerDados.php
@@ -1,27 +1,40 @@
 <?php
 require_once '../conexao/conexao.php';
 
+// Inicia a sessão (se já não estiver iniciada)
+session_start();
+
 try {
-        $sql = "SELECT * FROM  usuario where matricula_usuario = 21295220;";
+    // Verifica se o email está definido na sessão
+    if (isset($_SESSION['email'])) {
+        $email = $_SESSION['email'];
+
+        // Prepara a consulta usando um placeholder para o email
+        $sql = "SELECT * FROM usuario WHERE email = :email";
 
         $stmt = $conexao->prepare($sql);
-    
+
+        // Atribui o valor do email ao placeholder na consulta
+        $stmt->bindParam(':email', $email);
+
         $stmt->execute();
-    
+
         $result = $stmt->fetchAll();
 
-        
-
-        // $result = $result[0];
-
-        echo $result[0][3];
-    
-
-}catch (PDOException $e) {
-    echo '<script>alert("Usuário ou senha inválidos!"); window.location="login.html";</script>';;
+        if ($stmt->rowCount() == 1) {
+            foreach ($result as $row) {
+                foreach ($row as $key => $value) {
+                    echo $key . ': ' . $value . '<br>'; // Mostra chave e valor
+                }
+                echo '<br>'; // Quebra para a próxima linha
+            }
+        } else {
+            echo "Nenhum usuário encontrado com esse email.";
+        }
+    } else {
+        echo "O email não está definido na sessão.";
+    }
+} catch (PDOException $e) {
     die($e->getMessage());
 }
-
-die();
-
 ?>
